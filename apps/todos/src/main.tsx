8058979a4518f3d86424d4d41a20e1bdf4fce98d
Philip Johnston <stackshuffle@gmail.com>
2020-09-21 13:05:57 -0600

    feat(apps): 💄  Add reflexjs to all apps

--- apps/todos/src/main.tsx
+++ apps/todos/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'theme-ui';
+import theme from '@reflexjs/preset-base';
 
 import App from './app/app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
